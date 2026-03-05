454a6ec265b414c295c2c5dff7820c9875ac4809
V K <koneru.chowdary@gmail.com>
2026-03-05 21:27:35 +0100

    ui: use date-fns format in RelativeTime tooltip

--- src/components/relative-time.tsx
+++ src/components/relative-time.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { formatDistanceToNow } from 'date-fns'
+import { format, formatDistanceToNow } from 'date-fns'
 import { TooltipContent, TooltipRoot, TooltipTrigger } from '@/components/ui/tooltip'
 
 export function RelativeTime({ ms }: { ms: number }) {
@@ -10,7 +10,7 @@ export function RelativeTime({ ms }: { ms: number }) {
       <TooltipTrigger>
         <span style={{ cursor: 'default' }}>{formatDistanceToNow(date, { addSuffix: true })}</span>
       </TooltipTrigger>
-      <TooltipContent>{date.toLocaleString()}</TooltipContent>
+      <TooltipContent>{format(date, 'do MMMM yyyy, p')}</TooltipContent>
     </TooltipRoot>
   )
 }
